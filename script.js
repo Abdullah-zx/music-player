8da5b0ceb0ef3aed7a9f582a9035d8b1992ec7d8
Abdullah-zx <qamarabdullahabdullah716@gmail.com>
2026-08-19 18:46:09 +0500

    "Debug Spotify connection"

--- script.js
+++ script.js
@@ -589,15 +589,13 @@
 
       
     player.connect().then(success => {
+  console.log("SPOTIFY CONNECT RESULT:", success);
 
   if (success) {
-    console.log("Spotify SDK connected successfully");
     statusLine.textContent = "Spotify Connected";
   } else {
-    console.error("Spotify SDK connection failed");
     statusLine.textContent = "Spotify connection failed";
   }
-
 });
 
     };
